9c12027bb56c354ebdb52f7e84b4705d6c19da6e
growingdever <growingdever@gmail.com>
2016-05-29 17:11:12 +0900

    API 구현을 `flask-restful`을 쓰는 것으로 변경

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify
 from flask.ext.cors import CORS
+from flask.ext.restful import Api, Resource
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import NotFound
 
 app = Flask(__name__, static_folder='static')
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://{0}:{1}@{2}/{3}".format('remembered',
+api_root = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{0}:{1}@{2}/{3}'.format('remembered',
                                                                                  'growingdever',
                                                                                  'localhost:3306',
                                                                                  'remembered')
@@ -21,7 +24,6 @@ from app.models.labelling import LabellingModel
 
 db.create_all()
 
-
 from app.utils.model import model_to_dict
 
 
@@ -30,37 +32,42 @@ def index():
     return 'Hello, World!'
 
 
-@app.route('/api/notes')
-def notes():
-    return jsonify({
-        'items': [model_to_dict(item) for item in NoteModel.query.all()]
-    })
+@api_root.resource('/api/notes')
+class APINotes(Resource):
+    def get(self):
+        return jsonify({
+            'items': [model_to_dict(item) for item in NoteModel.query.all()]
+        })
 
 
-@app.route('/api/note/<int:note_id>')
-def note(note_id):
-    item = NoteModel.query.filter(NoteModel.id == note_id).first()
-    if item is None:
-        raise NotFound
+@api_root.resource('/api/notes/<int:note_id>')
+class APINote(Resource):
+    def get(self, note_id):
+        print(note_id)
+        item = NoteModel.query.filter(NoteModel.id == note_id).first()
+        if item is None:
+            raise NotFound
 
-    return jsonify({
-        'item': model_to_dict(item)
-    })
-
-
-@app.route('/api/labels')
-def labels():
-    return jsonify({
-        'items': [model_to_dict(item) for item in LabelModel.query.all()]
-    })
+        return jsonify({
+            'item': model_to_dict(item)
+        })
 
 
-@app.route('/api/label/<int:label_id>')
-def label(label_id):
-    item = LabelModel.query.filter(LabelModel.id == label_id).first()
-    if item is None:
-        raise NotFound
+@api_root.resource('/api/labels')
+class APILabels(Resource):
+    def get(self):
+        return jsonify({
+            'items': [model_to_dict(item) for item in LabelModel.query.all()]
+        })
 
-    return jsonify({
-        'item': model_to_dict(item)
-    })
+
+@api_root.resource('/api/labels/<int:label_id>')
+class APILabel(Resource):
+    def get(self, label_id):
+        item = LabelModel.query.filter(LabelModel.id == label_id).first()
+        if item is None:
+            raise NotFound
+
+        return jsonify({
+            'item': model_to_dict(item)
+        })
