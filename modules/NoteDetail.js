ae9dc474d640637d6a178253e181dcfa83781a8e
growingdever <growingdever@gmail.com>
2016-05-30 23:25:38 +0900

    노트 상세 view에 타이틀 부분 UI 추가

--- modules/NoteDetail.js
+++ modules/NoteDetail.js
@@ -9,9 +9,19 @@ export default React.createClass({
   render() {
     return (
       <div className="eight wide column">
-        <h1>
-          hello
-        </h1>
+        <div className="ui grid note detail top">
+          <div className="twelve wide column">
+            <div className="ui huge transparent fluid input">
+              <input type="text" placeholder="제목을 입력해주세요"/>
+            </div>
+          </div>
+          <div className="right floated four wide column">
+            <div className="mini right floated ui buttons">
+              <button className="ui green basic button">수정</button>
+              <button className="ui red basic button">삭제</button>
+            </div>
+          </div>
+        </div>
         <RichStyleEditor onChangeContent={this.onChangeEditorContent}/>
       </div>
     )
