6e6e4e16344eeb39f9434dce7f251208bfe8b53f
growingdever <growingdever@gmail.com>
2016-05-30 15:45:19 +0900

    메모 생성 됨

--- modules/Notes.js
+++ modules/Notes.js
@@ -23,15 +23,56 @@ export default React.createClass({
       }.bind(this)
     });
   },
+  handleNewNoteTitleChange(e) {
+    this.setState({new_note_title: e.target.value});
+  },
+  handleNewNoteContentChange(e) {
+    this.setState({new_note_content: e.target.value});
+  },
+  createNote(e) {
+    var url = 'http://localhost:5000/api/notes';
+    jQuery.ajax({
+      url: url,
+      method: 'POST',
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        title: this.state.new_note_title,
+        content: this.state.new_note_content,
+        label_ids: []
+      }),
+      cache: false,
+      success: function (response) {
+        this.setState({data: response.items});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render() {
     return (
       <div>
         <h2>Notes</h2>
+
+        <input
+          type="text"
+          placeholder="New note title"
+          value={this.state.new_note_title}
+          onChange={this.handleNewNoteTitleChange}/>
+        <textarea
+          type="text"
+          placeholder="New note content"
+          value={this.state.new_note_content}
+          onChange={this.handleNewNoteContentChange}/>
+        <button type="button" onClick={this.createNote}>Create</button>
+
         <ul>
           {this.state.data.map(function (note) {
             return <Note key={note.id} data={note}/>;
           })}
         </ul>
+
         {this.props.children}
       </div>
     )
