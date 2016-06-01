931220d2887d13566a43569436366f324d2498a0
growingdever <growingdever@gmail.com>
2016-06-02 01:43:33 +0900

    no message

--- modules/App.js
+++ modules/App.js
@@ -1,16 +1,9 @@
 import React from 'react'
-import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
     return (
       <div className="ui container">
-        <ul role="nav">
-          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-          <li><NavLink to="/notes/1">Notes</NavLink></li>
-        </ul>
-
         <div style={{"marginTop": "10px"}}>
           {this.props.children}
         </div>
